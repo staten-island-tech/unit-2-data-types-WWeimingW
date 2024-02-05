ec20641ab85ef1e0671f385287373fc70fc7389a
WWeimingW <weimingw@nycstudents.net>
2024-02-05 12:28:58 -0500

    sw

--- data.py
+++ data.py
@@ -22,11 +22,33 @@ def is_tenured(status):
 
 is_tenured(tenure) """
 
-values = [1,2.23,5,7,2,30,15]
+""" values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
     print(i)
 
 print(values[0])
 print(values[6])
+ """
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
 
+day_of_week = input("what day is it")
+day_of_week = Friday
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") 
+
+""" temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')"""
+ 
+#if the temp is higher then 68, it will state that it's warm, exactly 68 is perfect, below 68 is cold
