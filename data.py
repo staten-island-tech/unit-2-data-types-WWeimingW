e171bf82f5d7c9bb8a2b6337ad312c527744f279
WWeimingW <weimingw@nycstudents.net>
2024-02-06 12:18:43 -0500

    s

--- data.py
+++ data.py
@@ -36,8 +36,8 @@ z = y[0]
 print(y)
 print(z) """
 
-day_of_week = input("what day is it")
-day_of_week = Friday
+day_of_week = input("What day is it")
+day_of_week == "Friday"
 if day_of_week == "Friday":
     print("correct")
 else:
